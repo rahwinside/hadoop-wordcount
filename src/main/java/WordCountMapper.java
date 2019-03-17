b631d3bd963d6b12955dcd5389e57e765bf8b6d9
Kallen Tu <kallentu@hotmail.ca>
2019-03-17 03:42:04 -0400

    Initial Word Count MapReduce project.

---
+++ src/main/java/WordCountMapper.java
@@ -0,0 +1,24 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+    @Override
+    public void map(Object key, Text input, Context context) {
+        try {
+            StringTokenizer tokenizer = new StringTokenizer(input.toString());
+            while (tokenizer.hasMoreElements()) {
+                Text word = new Text();
+                word.set(tokenizer.nextToken());
+                context.write(word, new IntWritable(1));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/WordCountReducer.java
@@ -0,0 +1,24 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) {
+        try {
+            int resultNumber = 0;
+            for (IntWritable value : values) {
+                resultNumber += value.get();
+            }
+            IntWritable result = new IntWritable();
+            result.set(resultNumber);
+            context.write(key, result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
